d7d2fc54c548633e80c737d3a1026b932f2380a5
zdziechoo <piotr.zdziechowski@gmail.com>
2016-11-13 18:33:20 +0100

    exception CarrierAlreadyRentException implemented still to do main (inteactive)

--- Movie Rental/src/com/zdziechowski/movierental/carrier/CarrierAlreadyRentException.java
+++ Movie Rental/src/com/zdziechowski/movierental/carrier/CarrierAlreadyRentException.java
@@ -1,5 +1,11 @@
 package com.zdziechowski.movierental.carrier;
 
 public class CarrierAlreadyRentException extends RuntimeException {
-	
+    private static String CARRIERMESSAGE = "Carrier already rent";
+
+    public static String getCarrierAlreadyRent() {
+        return CARRIERMESSAGE;
+    }
+
+
 }
